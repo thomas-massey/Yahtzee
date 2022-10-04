fc68bab0548570aff813f868bc8f32f88a2c4fd5
Thomas Massey <thomassey476@outlook.com>
2022-10-04 23:16:14 +0100

    Boilerplate

---
+++ GameEngine.py
@@ -0,0 +1,5 @@
+# This is the game engine for a game of Yahtzee in OOP making use of Pygame
+
+import RenderEngine
+
+class GameEngine:
---
+++ main.py
@@ -0,0 +1,6 @@
+# This is a game of Yahtzee in OOP making use of Pygame
+
+import GameEngine
+
+if __name__ == "__main__":
+    game = GameEngine.GameEngine()
