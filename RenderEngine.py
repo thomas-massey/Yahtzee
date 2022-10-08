ee059ad0d5d6bc9fbb9fbcced99f89b5289edd91
Thomas Massey <thomassey476@outlook.com>
2022-10-08 17:19:51 +0100

    Got scorebaord showing

--- RenderEngine.py
+++ RenderEngine.py
@@ -1,5 +1,6 @@
 # This is the render engine for a game of Yahtzee in OOP making use of Pygame
 
+from time import sleep
 import pygame
 
 class RenderEngine:
@@ -38,47 +39,35 @@ class RenderEngine:
         # etc
         # Total 0 0
         
-        # Clear the screen
-        self.screen.fill(self.GREEN)
-        # Draw the table in the extra 200 pixels from the self.WIDTH NOT self.GAME_WIDTH
-        pygame.draw.rect(self.screen, self.BLACK, (self.WIDTH, 0, 200, self.HEIGHT))
-        # Draw the lines
-        pygame.draw.line(self.screen, self.BLACK, (self.WIDTH, 0), (self.WIDTH, self.HEIGHT), 2)
-        pygame.draw.line(self.screen, self.BLACK, (self.WIDTH, 0), (self.WIDTH + 200, 0), 2)
-        pygame.draw.line(self.screen, self.BLACK, (self.WIDTH + 100, 0), (self.WIDTH + 100, self.HEIGHT), 2)
-        pygame.draw.line(self.screen, self.BLACK, (self.WIDTH, self.HEIGHT), (self.WIDTH + 200, self.HEIGHT), 2)
-        # Draw the text
+        # Render the table by drawing a grid of lines - drawing them to the right of the game in teh self.WIDTH area (300px)
+        # Draw the vertical lines
+        for i in range(1, 3):
+            pygame.draw.line(self.screen, self.BLACK, (self.GAME_WIDTH + (i * 100), 0), (self.GAME_WIDTH + (i * 100), self.HEIGHT), 1)
+        # Draw the horizontal lines
+        for i in range(1, 14):
+            pygame.draw.line(self.screen, self.BLACK, (self.GAME_WIDTH, i * 30), (self.WIDTH, i * 30), 1)
+        # Render the text
         text = pygame.font.Font(None, 36)
         text_surface = text.render("Player 1", True, self.BLACK)
-        text_rect = text_surface.get_rect(center=(self.WIDTH + 50, 20))
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH + 150, 15))
         self.screen.blit(text_surface, text_rect)
         text_surface = text.render("Player 2", True, self.BLACK)
-        text_rect = text_surface.get_rect(center=(self.WIDTH + 150, 20))
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH + 250, 15))
         self.screen.blit(text_surface, text_rect)
-        # # Draw the scores
-        # y = 50
-        # for score in scores:
-        #     text_surface = text.render(score, True, self.BLACK)
-        #     text_rect = text_surface.get_rect(center=(self.WIDTH + 50, y))
-        #     self.screen.blit(text_surface, text_rect)
-        #     text_surface = text.render(str(scores[score][0]), True, self.BLACK)
-        #     text_rect = text_surface.get_rect(center=(self.WIDTH + 50, y))
-        #     self.screen.blit(text_surface, text_rect)
-        #     text_surface = text.render(str(scores[score][1]), True, self.BLACK)
-        #     text_rect = text_surface.get_rect(center=(self.WIDTH + 150, y))
-        #     self.screen.blit(text_surface, text_rect)
-        #     y += 30
-        # # Draw the totals
-        # text_surface = text.render("Total", True, self.BLACK)
-        # text_rect = text_surface.get_rect(center=(self.WIDTH + 50, y))
-        # self.screen.blit(text_surface, text_rect)
-        # text_surface = text.render(str(scores["Total"][0]), True, self.BLACK)
-        # text_rect = text_surface.get_rect(center=(self.WIDTH + 50, y))
-        # self.screen.blit(text_surface, text_rect)
-        # text_surface = text.render(str(scores["Total"][1]), True, self.BLACK)
-        # text_rect = text_surface.get_rect(center=(self.WIDTH + 150, y))
-        # self.screen.blit(text_surface, text_rect)
-        print("Hi")
+        # Render the scores
+        i = 1
+        for key in scores:
+            text_surface = text.render(key, True, self.BLACK)
+            text_rect = text_surface.get_rect(center=(self.GAME_WIDTH + 50, i * 30 + 15))
+            self.screen.blit(text_surface, text_rect)
+            text_surface = text.render(str(scores[key][0]), True, self.BLACK)
+            text_rect = text_surface.get_rect(center=(self.GAME_WIDTH + 150, i * 30 + 15))
+            self.screen.blit(text_surface, text_rect)
+            text_surface = text.render(str(scores[key][1]), True, self.BLACK)
+            text_rect = text_surface.get_rect(center=(self.GAME_WIDTH + 250, i * 30 + 15))
+            self.screen.blit(text_surface, text_rect)
+            i += 1
+
         pygame.display.update()
 
     def quit(self):
@@ -103,7 +92,7 @@ class RenderEngine:
     def CONSTANTS(self):
         self.HEIGHT = 480
         self.GAME_WIDTH = 640
-        self.WIDTH = self.GAME_WIDTH + 200
+        self.WIDTH = self.GAME_WIDTH + 300
         self.FPS = 60
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
