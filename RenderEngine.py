e4f5ac2ceb26929494904d6bad56ef372e84b56a
Thomas Massey <thomassey476@outlook.com>
2022-10-09 00:38:34 +0100

    Working GUI

--- RenderEngine.py
+++ RenderEngine.py
@@ -15,7 +15,7 @@ class RenderEngine:
 
     def load_screen(self):
         self.screen.fill(self.WHITE)
-        text = pygame.font.Font(None, 36)
+        text = pygame.font.Font(None, 50)
         text_surface = text.render("Yahtzee", True, self.BLACK)
         text_rect = text_surface.get_rect(center=(self.WIDTH/2, self.HEIGHT/2))
         self.screen.blit(text_surface, text_rect)
@@ -26,10 +26,23 @@ class RenderEngine:
         self.screen.fill(self.WHITE)
         pygame.display.update()
 
-    def render_game(self, game, scores):
+    def render_game(self, game, scores, player):
         self.scores = scores
         self.screen.fill(self.GREEN)
         self.render_scores(scores)
+        self.render_player_turn(player) # Not OOP :(
+
+    def render_player_turn(self, player):
+        if player:
+            player = "Player 1"
+        else:
+            player = "Player 2"
+        # Render the text on the top of the screen saying whose turn it is
+        text = pygame.font.Font(None, 36)
+        text_surface = text.render("Player " + str(player) + "'s turn", True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH/2, 15))
+        self.screen.blit(text_surface, text_rect)
+        pygame.display.update()
         
     def render_scores(self, scores):
         # Render the scores on the side in a table like:
@@ -79,7 +92,8 @@ class RenderEngine:
     def roll_dice(self, dice, number_of_dice):
         self.number_of_dice = number_of_dice
         # Roll the dice
-        self.screen.fill(self.GREEN)
+        # Fill where all 5 dice are with green
+        self.screen.fill(self.GREEN, (0, self.HEIGHT - 100, self.number_of_dice * 100, 100))
         self.render_scores(self.scores)
         # The way this works is there are 5 static dice and it will roll all the dice (staying still - updating the image)
         # and stop one by one.
@@ -103,10 +117,11 @@ class RenderEngine:
 
             self.update_display() # Prevent not responding
 
-            sleep(0.1)
+            sleep(0.05)
             pygame.display.update()
 
-    def get_input(self):
+    def get_input(self, dice_round):
+        self.dice_round = dice_round
         # Draw confimation buttons
         self.draw_stop_button()
         self.remove_dice = []
@@ -119,11 +134,71 @@ class RenderEngine:
                     mouse_pos = event.pos
                     # Check if the mouse is in the area of the dice
                     if mouse_pos[0] < self.number_of_dice * 100 and mouse_pos[1] > self.HEIGHT - 100:
-                        # Check which dice it is
-                        self.remove_dice.append(mouse_pos[0] // 100)
+                        # Make sure it has not been removed already
+                        if mouse_pos[0] // 100 not in self.remove_dice:
+                            # Check which dice it is
+                            self.remove_dice.append(mouse_pos[0] // 100)
+                            # Add a blue dot ontop of the dice
+                            pygame.draw.circle(self.screen, self.BLUE, ((mouse_pos[0] // 100) * 100 + 50, self.HEIGHT - 150), 10) # Hardcoded AAAGGHHH
+                            pygame.display.update()
+                        else:
+                            # Remove the dot
+                            self.remove_dice.remove(mouse_pos[0] // 100)
+                            pygame.draw.circle(self.screen, self.GREEN, ((mouse_pos[0] // 100) * 100 + 50, self.HEIGHT - 150), 10) # Hardcoded AAAGGHHH
+                            pygame.display.update()
                     if (mouse_pos[0] > self.GAME_WIDTH - 100) and mouse_pos[0] < self.GAME_WIDTH and mouse_pos[1] > self.HEIGHT - 100:
                         # Check if the stop button was pressed
-                        print(self.remove_dice)
+                        # If round 3 - return all left over dice
+                        if self.dice_round == 1:
+                            self.remove_dice = []
+                            for i in range(0, self.number_of_dice):
+                                self.remove_dice.append(i)
+                        return self.remove_dice
+                    
+    def game_over(self, winner, scores):
+        # Render the winner
+        self.screen.fill(self.WHITE)
+        text = pygame.font.Font(None, 36)
+        text_surface = text.render("Game Over", True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH // 2, 15))
+        self.screen.blit(text_surface, text_rect)
+        text_surface = text.render("Winner: " + winner, True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH // 2, 50))
+        self.screen.blit(text_surface, text_rect)
+        # Render the scores
+        self.render_scores(scores)
+        pygame.display.update()
+        # Wait 5 seconds - ask the user if they want to play again
+        sleep(5)
+        self.screen.fill(self.WHITE)
+        text = pygame.font.Font(None, 36)
+        text_surface = text.render("Play Again?", True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH // 2, 15))
+        self.screen.blit(text_surface, text_rect)
+        # Now draw the buttons
+        pygame.draw.rect(self.screen, self.GREEN, (self.GAME_WIDTH // 2 - 100, 50, 100, 50))
+        pygame.draw.rect(self.screen, self.RED, (self.GAME_WIDTH // 2, 50, 100, 50))
+        text = pygame.font.Font(None, 36)
+        text_surface = text.render("Yes", True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH // 2 - 50, 75))
+        self.screen.blit(text_surface, text_rect)
+        text_surface = text.render("No", True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH // 2 + 50, 75))
+        self.screen.blit(text_surface, text_rect)
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    self.quit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    if mouse_pos[0] > self.GAME_WIDTH // 2 - 100 and mouse_pos[0] < self.GAME_WIDTH // 2 and mouse_pos[1] > 50 and mouse_pos[1] < 100:
+                        # Play again
+                        return True
+                    if mouse_pos[0] > self.GAME_WIDTH // 2 and mouse_pos[0] < self.GAME_WIDTH // 2 + 100 and mouse_pos[1] > 50 and mouse_pos[1] < 100:
+                        # Quit
+                        return False
 
     def draw_stop_button(self):
         # In the gap between the dice and the scores, draw a stop button
@@ -131,10 +206,66 @@ class RenderEngine:
         pygame.draw.rect(self.screen, self.RED, (self.GAME_WIDTH - 100, self.HEIGHT - 100, 100, 100))
         # Draw the text
         text = pygame.font.Font(None, 36)
-        text_surface = text.render("Stop", True, self.WHITE)
+        if self.dice_round == 1:
+            text_surface = text.render("End", True, self.BLACK)
+        else:
+            text_surface = text.render("Next", True, self.WHITE)
         text_rect = text_surface.get_rect(center=(self.GAME_WIDTH - 50, self.HEIGHT - 50))
         self.screen.blit(text_surface, text_rect)
         pygame.display.update()
+
+    def show_saved_dice(self, kept_dice):
+        self.kept_dice = kept_dice
+        # Show the dice that were saved on the right side of the screen vertically before the scores
+        for i in range(0, len(self.kept_dice)):
+            self.screen.blit(self.dice[self.kept_dice[i]-1], (self.GAME_WIDTH - 100, i * 100))
+        pygame.display.update()
+
+    def select_score_category(self, player_1_turn, scores, unavailable_categories):
+        if player_1_turn:
+            self.player = 0
+            self.player_number = 1
+        else:
+            self.player = 1
+            self.player_number = 2
+        self.unavailable_categories = unavailable_categories
+        self.scores = scores
+        # Clear the screen - show the scores to the right
+        self.screen.fill(self.GREEN)
+        # Draw the dice at the top of the screen
+        for i in range(0, len(self.kept_dice)):
+            self.screen.blit(self.dice[self.kept_dice[i]-1], (i * 100, 0))
+        # Draw the score categories in a grid
+        # Draw the text
+        text = pygame.font.Font(None, 36)
+        text_surface = text.render(f"Player {self.player_number}, please select a score category", True, self.BLACK)
+        text_rect = text_surface.get_rect(center=(self.GAME_WIDTH / 2, self.HEIGHT / 2))
+        self.screen.blit(text_surface, text_rect)
+        # Draw the score categories
+        # Add a red background to the score categories that are not available
+        for j in range(1, len(scores)):
+            if j in self.unavailable_categories:
+                # Draw rectangle from score categories name top left to bottom right
+                pygame.draw.rect(self.screen, self.RED, (self.GAME_WIDTH, j * 30 +30, 100, 30))
+        # Render the score categories over the red background
+        self.render_scores(self.scores)
+        pygame.display.update()
+        return self.get_score_category()
+
+    def get_score_category(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    self.quit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    # Check if the mouse is in the area of the score categories
+                    # Check which score category it is from the score part of the screen                  
+                    if mouse_pos[0] > self.GAME_WIDTH and mouse_pos[1] < self.HEIGHT:
+                        # Check if the score category is available
+                        if mouse_pos[1] // 30 - 1 not in self.unavailable_categories and mouse_pos[1] < 30 * 14: # Hardcoded - D'oh!
+                            return mouse_pos[1] // 30 - 1
 
     def update_display(self):
         for event in pygame.event.get():
@@ -163,7 +294,7 @@ class RenderEngine:
             self.dice.append(pygame.image.load("images/dice_" + str(i) + ".bmp"))
 
     def CONSTANTS(self):
-        self.HEIGHT = 480
+        self.HEIGHT = 500
         self.GAME_WIDTH = 640
         self.WIDTH = self.GAME_WIDTH + 300
         self.FPS = 60
